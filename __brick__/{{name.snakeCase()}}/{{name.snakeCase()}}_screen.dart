79d73d6c6075990ae9400d6d4aa4677c316ca32b
Adi Wirya <50317150+guardling7@users.noreply.github.com>
2023-09-13 13:27:14 +0700

    add controller

--- __brick__/{{name.snakeCase()}}/{{name.snakeCase()}}_screen.dart
+++ __brick__/{{name.snakeCase()}}/{{name.snakeCase()}}_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-
+import 'controllers/{{name.snakeCase()}}_controller.dart';
 class {{name.pascalCase()}}Screen extends GetView<{{name.pascalCase()}}Controller> {
 const {{name.pascalCase()}}Screen({super.key});
 
